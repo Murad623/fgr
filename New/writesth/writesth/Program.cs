de877dd0726645735e6172bf59d99bdf27fbd1c4
Murad623 <muradmehdiyev79@gmail.com>
2024-09-13 14:20:19 +0400

    new code

---
+++ New/writesth/writesth/Program.cs
@@ -0,0 +1,10 @@
+﻿string characterName = "Mario";
+int characterAge = 43;
+string nationality = "Italian";
+float height = 1.55f;
+decimal weight = 64.41m;
+
+Console.WriteLine($"It's me, {characterName}. I'm {characterAge} years old." +
+    $"\nI must save my Princess. She can't escape the tower." +
+    $"\nI'm just {height} m and {weight} kg." +
+    $"\nOh, I almost forgot. I came from {nationality} to save my princess.");
