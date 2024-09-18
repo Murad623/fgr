6393ba14f1b29671c7d644f704d086934ac8e4f5
Murad623 <muradmehdiyev79@gmail.com>
2024-09-18 13:35:16 +0400

    18sep

---
+++ 15sep/15sep/Program.cs
@@ -0,0 +1,50 @@
+﻿#region 1
+//string[] names = { "Murad", "Nergiz", "Nicat", "Nigar" };
+////name = Console.ReadLine();
+//for (int i = names.Length - 1; i >= 0; i--)
+//{
+//	for (int j = names[i].Length - 1; j >= 0; j--)
+//		Console.Write(names[i][j]);
+//	Console.Write(" ");
+//}
+#endregion
+
+#region 2
+//int[] nums = { 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 24, 38, 36, 42 };
+//for (int i = 0; i < nums.Length; i++)
+//    if (nums[i] % 2 == 0 && nums[i] % 3 == 0)
+//        Console.WriteLine(nums[i]);
+#endregion
+
+#region 3
+//int[] nums1 = { 1, 4, 3, 7, 5};
+//int[] nums2 = { 3, 6, 9, 0, 2 };
+
+//int[] nums = new int[nums1.Length + nums2.Length];
+//for (int i = 0; i < nums1.Length; i++)
+//    nums[i] = nums1[i];
+//for (int i = 0; i < nums2.Length; i++)
+//    nums[i + nums1.Length] = nums2[i];
+//Array.Sort(nums);
+//for (int i = 0; i < nums.Length; i++)
+//    Console.Write(nums[i] + " ");
+#endregion
+
+#region 4
+//int a = 196;
+//if (a > 1)
+//{
+//    for (int b = 0; b < a/2; b++)
+//    {
+//        if (b *b  == a)
+//        {
+//            Console.WriteLine(b);
+//            break;
+//        }
+//    }
+//}
+//else
+//{
+//    Console.WriteLine(a);
+//}
+#endregion
---
+++ 18sep/18sep/Program.cs
@@ -0,0 +1,46 @@
+﻿#region uzunluğu 4-dən böyük olan sözlər
+Console.WriteLine("---------------------------------------------------------");
+string[] words = { "dəniz", "ada", "palma", "kakos", "daş", "od", "balıq" };
+for (int i = 0; i < words.Length; i++)
+{
+    if (words[i].Length > 4)
+    {
+        Console.WriteLine(words[i]);
+    }
+}
+Console.WriteLine("---------------------------------------------------------\n");
+#endregion
+
+#region tərs & cüt
+int[] nums = { 3, 4, 5, 1, 2, 7, 8, 6, 9, 0, 10, 15, 14, 13, 12, 11, 18, 17 };
+Array.Reverse(nums);
+for (int i = 0;i < nums.Length; i++)
+{
+    if (nums[i] > 1)
+    {
+        if (nums[i]%2 == 0)
+        {
+            Console.Write(nums[i] + " ");
+        }
+    }
+}
+Console.WriteLine("\n---------------------------------------------------------\n");
+#endregion
+
+#region palindrom
+string[] words2 = { "yol", "maşın", "sürət", "radar", "ata", "mesaj", "açar"};
+for (int i = 0; i < words2.Length; i++)
+{
+    string reverse = "";
+    char[] reverseWA = words2[i].ToCharArray();
+    for (int j = reverseWA.Length - 1; j >= 0; j--)
+    {
+        reverse += reverseWA[j];
+    }
+    if (reverse == words2[i])
+    {
+        Console.WriteLine(reverse);
+    }
+}
+Console.WriteLine("---------------------------------------------------------\n");
+#endregion
