e6ecd457bfeef96f077361e838f1f68e27b17792
Murad623 <muradmehdiyev79@gmail.com>
2024-11-29 00:38:22 +0400

    Age Problem

---
+++ AgeProblem/AgeProblem/Classes/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace AgeProblem.Classes
+{
+    internal class Student
+    {
+        static int _Age = 0;
+        public int Age = 0;
+        public Student()
+        {
+            _Age++;
+            Age = _Age;
+        }
+    }
+}
---
+++ AgeProblem/AgeProblem/Program.cs
@@ -0,0 +1,7 @@
+﻿using AgeProblem.Classes;
+Student student1 = new Student();
+Student student2 = new Student();
+Student student3 = new Student();
+Console.WriteLine(student1.Age);
+Console.WriteLine(student2.Age);
+Console.WriteLine(student3.Age);
